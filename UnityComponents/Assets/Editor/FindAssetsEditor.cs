a4205414b87e5747a2008a10e66171e7a44eb865
saeipi <ksert@163.com>
2019-12-26 15:19:41 +0800

    项目初始化

---
+++ UnityComponents/Assets/Editor/FindAssetsEditor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(DefaultAsset))]
+public class FindAssetsEditor : Editor
+{
+    private string filter;
+    private string[] searchInFolders;
+
+    private string[] assets;
+    void Awake()
+    {
+        var folders = new List<string>();
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            string assetPath = AssetDatabase.GetAssetPath(targets[i]);
+            if (Directory.Exists(assetPath))
+                folders.Add(assetPath);
+        }
+
+        searchInFolders = folders.ToArray();
+    }
+
+    void OnDestroy()
+    {
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GUI.enabled = searchInFolders.Length == targets.Length;
+        if (!GUI.enabled)
+            return;
+
+        filter = EditorGUILayout.TextField(filter);
+
+        if (GUILayout.Button("查找"))
+        {
+            assets = AssetDatabase.FindAssets(filter, searchInFolders);
+        }
+
+        for (int i = 0; assets != null && i < assets.Length; i++)
+        {
+            var assetPath = AssetDatabase.GUIDToAssetPath(assets[i]);
+            if (GUILayout.Button(new GUIContent(assetPath, AssetDatabase.GetCachedIcon(assetPath)), "Label"))
+            {
+                var asset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+                Selection.activeObject = asset;
+            }
+        }
+    }
+}
---
+++ UnityComponents/Assets/Editor/KSPrefabComponmentEditor.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class KSPrefabComponmentEditor
+{
+    [MenuItem("KSMenu/Export Prefab")]
+    static void PrefabComponment()
+    {
+        GameObject target = Selection.activeTransform.gameObject;
+
+        ArrayList prefabs = new ArrayList();
+        ArrayList images = new ArrayList();
+        ArrayList scripts = new ArrayList();
+
+        prefabs.Add(target.name);
+
+        Transform[] transforms = target.GetComponentsInChildren<Transform>();
+        foreach (var child in transforms)
+        {
+            Image image = child.gameObject.GetComponent<Image>();
+            if (image != null)
+            {
+                images.Add(image.sprite.name);
+            }
+            foreach (var component in child.GetComponents<Component>())
+            {
+                string componentType = component.GetType().ToString();
+                if (componentType.StartsWith("UnityEngine") == false)
+                {
+                    scripts.Add(componentType);
+                }
+            }
+        }
+
+        ArrayList prefabsPath = GetAssetPaths(prefabs, "prefab");
+        ArrayList imagesPath = GetAssetPaths(images, "png");
+        ArrayList scriptsPath = GetAssetPaths(scripts, "cs");
+
+        string exportPath = @"/Volumes/data/UnityApp/NewCopyProject/";
+#if UNITY_STANDALONE_WIN
+        exportPath = exportPath.Replace(@"\", "/");
+#endif
+
+        bool overwrite = true;
+        foreach (string path in prefabsPath)
+        {
+            ExportAssets(path, exportPath, overwrite);
+        }
+        foreach (string path in imagesPath)
+        {
+            ExportAssets(path, exportPath, overwrite);
+        }
+        foreach (string path in scriptsPath)
+        {
+            ExportAssets(path, exportPath, overwrite);
+        }
+        Debug.Log("执行完毕");
+    }
+
+    static ArrayList GetAssetPaths(ArrayList assets, string type)
+    {
+        ArrayList paths = new ArrayList();
+        foreach (string filter in assets)
+        {
+            string[] resules = AssetDatabase.FindAssets(filter);
+            for (int i = 0; i < resules.Length; i++)
+            {
+                var assetPath = GetAssetPath(resules[i], filter, type);
+                if (assetPath != string.Empty)
+                {
+                    paths.Add(assetPath);
+                }
+            }
+        }
+        return paths;
+    }
+    static string GetAssetPath(string guid, string filter, string type)
+    {
+        var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+        if (assetPath.EndsWith("/" + filter + "." + type))
+        {
+            return assetPath;
+        }
+        return string.Empty;
+    }
+
+    static void ExportAssets(string sourcePath, string exportPath, bool overwrite)
+    {
+        string[] strArray = sourcePath.Split('/');
+        if (strArray.Length == 0)
+        {
+            return;
+        }
+
+        string fileName = strArray[strArray.Length - 1];
+        exportPath = exportPath + sourcePath.Replace(fileName, "");
+        sourcePath = Application.dataPath + sourcePath.Replace("Assets", "");
+        if (!Directory.Exists(exportPath))
+        {
+            Directory.CreateDirectory(exportPath);
+        }
+        File.Copy(sourcePath, Path.Combine(exportPath, Path.GetFileName(fileName)), overwrite);
+        File.Copy(sourcePath + ".meta", Path.Combine(exportPath, Path.GetFileName(fileName + ".meta")), overwrite);
+    }
+}
