c2cc7d4352e56932d2a72e53d4819d1c707856d2
saeipi <ksert@163.com>
2019-12-26 17:20:34 +0800

    代码逻辑优化

--- UnityComponents/Assets/Editor/KSExportPrefabEditor.cs
+++ UnityComponents/Assets/Editor/KSExportPrefabEditor.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using System.Collections.Generic;
 
 public class KSExportPrefabEditor
 {
@@ -11,11 +12,11 @@ public class KSExportPrefabEditor
     {
         GameObject target = Selection.activeTransform.gameObject;
 
-        ArrayList prefabs = new ArrayList();
-        ArrayList images = new ArrayList();
-        ArrayList scripts = new ArrayList();
+        Dictionary<string, string> prefabs = new Dictionary<string, string>();
+        Dictionary<string, string> images = new Dictionary<string, string>();
+        Dictionary<string, string> scripts = new Dictionary<string, string>();
 
-        prefabs.Add(target.name);
+        InsetDictionary(prefabs, target.name, target.name);
 
         Transform[] transforms = target.GetComponentsInChildren<Transform>();
         foreach (var child in transforms)
@@ -23,47 +24,74 @@ public class KSExportPrefabEditor
             Image image = child.gameObject.GetComponent<Image>();
             if (image != null)
             {
-                images.Add(image.sprite.name);
+                InsetDictionary(images, image.sprite.name, image.sprite.name);
             }
             foreach (var component in child.GetComponents<Component>())
             {
                 string componentType = component.GetType().ToString();
                 if (componentType.StartsWith("UnityEngine") == false)
                 {
-                    scripts.Add(componentType);
+                    InsetDictionary(scripts, componentType, componentType);
                 }
             }
         }
 
-        ArrayList prefabsPath = GetAssetPaths(prefabs, "prefab");
-        ArrayList imagesPath = GetAssetPaths(images, "png");
-        ArrayList scriptsPath = GetAssetPaths(scripts, "cs");
+        Dictionary<string, string> prefabsPath = GetAssetPaths(prefabs, "prefab");
+        Dictionary<string, string> imagesPath = GetAssetPaths(images, "png");
+        Dictionary<string, string> scriptsPath = GetAssetPaths(scripts, "cs");
 
-        string exportPath = @"/Volumes/data/UnityApp/NewCopyProject/";
+        string exportPath = @"H:\UnityProject\CopyAss\";
 #if UNITY_STANDALONE_WIN
         exportPath = exportPath.Replace(@"\", "/");
 #endif
 
         bool overwrite = true;
-        foreach (string path in prefabsPath)
+        foreach (string path in prefabsPath.Keys)
         {
             ExportAssets(path, exportPath, overwrite);
         }
-        foreach (string path in imagesPath)
+        foreach (string path in imagesPath.Keys)
         {
             ExportAssets(path, exportPath, overwrite);
         }
-        foreach (string path in scriptsPath)
+        foreach (string path in scriptsPath.Keys)
         {
             ExportAssets(path, exportPath, overwrite);
         }
+
+        SaveExportList("ExportPrefabs", prefabsPath);
+        SaveExportList("ExportImages", imagesPath);
+        SaveExportList("ExportScripts", scriptsPath);
+
         Debug.Log("执行完毕");
     }
 
-    static ArrayList GetAssetPaths(ArrayList assets, string type)
+    static void Deduplication(ArrayList list)
     {
-        ArrayList paths = new ArrayList();
-        foreach (string filter in assets)
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                if (list[i].Equals(list[j]))
+                {
+                    list.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    static void InsetDictionary(Dictionary<string, string> ditc,string key,string value)
+    {
+        if(ditc.ContainsKey(key) == false)
+        {
+            ditc.Add(key, value);
+        }
+    }
+
+    static Dictionary<string, string> GetAssetPaths(Dictionary<string, string> assets, string type)
+    {
+        Dictionary<string, string> paths = new Dictionary<string, string>();
+        foreach (string filter in assets.Keys)
         {
             string[] resules = AssetDatabase.FindAssets(filter);
             for (int i = 0; i < resules.Length; i++)
@@ -71,7 +99,7 @@ public class KSExportPrefabEditor
                 var assetPath = GetAssetPath(resules[i], filter, type);
                 if (assetPath != string.Empty)
                 {
-                    paths.Add(assetPath);
+                    InsetDictionary(paths, assetPath, assetPath);
                 }
             }
         }
@@ -104,5 +132,38 @@ public class KSExportPrefabEditor
         }
         File.Copy(sourcePath, Path.Combine(exportPath, Path.GetFileName(fileName)), overwrite);
         File.Copy(sourcePath + ".meta", Path.Combine(exportPath, Path.GetFileName(fileName + ".meta")), overwrite);
+    }
+
+    static void SaveExportList(string filename, Dictionary<string, string> assets)
+    {
+        string savePath = Application.dataPath + "/Resources/ExportNotes/";
+
+        if (!Directory.Exists(savePath))
+        {
+            Directory.CreateDirectory(savePath);
+        }
+        savePath = savePath + filename + ".txt";
+
+        string context = string.Empty;
+        foreach (string key in assets.Keys)
+        {
+            context = context + key + "\r\n";
+        }
+
+        // 文件流创建一个文本文件
+        FileStream file = new FileStream(savePath, FileMode.Create);
+        //得到字符串的UTF8 数据流
+        byte[] bts = System.Text.Encoding.UTF8.GetBytes(context);
+        // 文件写入数据流
+        file.Write(bts, 0, bts.Length);
+        if (file != null)
+        {
+            //清空缓存
+            file.Flush();
+            // 关闭流
+            file.Close();
+            //销毁资源
+            file.Dispose();
+        }
     }
 }
